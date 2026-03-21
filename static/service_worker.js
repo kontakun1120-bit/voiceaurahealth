12b9be0beaabf7e991fe132cdf95fa573bf9c200
kontakun1120-bit <kontakun1120@gmail.com>
2026-03-21 15:37:33 +0900

    Add files via upload

--- static/service_worker.js
+++ static/service_worker.js
@@ -1,6 +1,24 @@
+const CACHE_NAME = "voiceaura-v1";
+
+const urlsToCache = [
+  "/",
+  "/static/main_logo.png",
+  "/static/mini_icon.png",
+  "/static/manifest.json"
+];
+
 self.addEventListener("install", function(event) {
-  console.log("Service Worker installed");
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
