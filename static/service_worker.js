415322c5ea25666a924d16b678f202d63f2e5219
kontakun1120-bit <kontakun1120@gmail.com>
2026-03-31 15:05:13 +0900

    Add files via upload

---
+++ static/service_worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "voiceaura-v3";
+
+const STATIC_ASSETS = [
+  "/",
+  "/static/main_logo.png",
+  "/static/mini_icon.png",
+  "/static/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(STATIC_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+
+  // APIはキャッシュしない
+  if (event.request.url.includes("/api/")) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
